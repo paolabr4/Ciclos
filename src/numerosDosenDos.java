cede6079dc691fde5754f7552fa4a14476e42d3a
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-06-25 08:52:50 -0500

    Numeros de 2 en 2

---
+++ src/numerosDosenDos.java
@@ -0,0 +1,11 @@
+public class numerosDosenDos {
+    public static void main(String[] args){
+        // imprimir 10 numeros sumados de 2 en 2, empezando desde 2
+
+        for (int i=2;i<=20; i=i+2){
+            System.out.println("numero: "+i);
+        }
+
+
+    }
+}
