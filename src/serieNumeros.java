6a47a91d63ad4307a85047e67a7057280fdabf93
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-06-25 09:05:52 -0500

    Serie de Numeros

---
+++ src/serieNumeros.java
@@ -0,0 +1,15 @@
+public class serieNumeros {
+
+        public static void main(String[] args){
+            // imprimir  numeros 1 3 1 3 1 3
+
+            for (int i=0;i<=2; i++){
+                System.out.println("1");
+                System.out.println("3");
+            }
+
+
+        }
+
+
+}
