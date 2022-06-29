2d196373394f4b82a3fa9ee36438233c51d79fe3
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-06-28 21:30:28 -0500

    Modificaciones finales

--- src/SerieFibonaci.java
+++ src/SerieFibonaci.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class SerieFibonaci {
-}
+import javax.swing.*;
+import java.net.StandardSocketOptions;
+
+public class SerieFibonaci {
+        public static void main(String[] args) {
+            int cont = 0;
+            int i = 0;
+            int j = 1;
+            int k = i+j;
+
+            System.out.println("Serie Fibonaci");
+            cont= Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos numeros de la serie desea generar.? "));
+
+            if (cont==1){
+                System.out.print(+i);
+            }
+           else if (cont==2){
+                System.out.print(+i);
+                System.out.print(" ,"+j);
+            }
+           else if (cont>=3) {
+                System.out.print(+i);
+                System.out.print(" ," + j);
+                System.out.print(" ," + k);
+
+                for (int c = 0; c < cont - 3; c++) {
+                    i = j;
+                    j = k;
+                    k = i + k;
+                    System.out.print(" ," + k);
+
+                }
+            }
+
+
+        }
+    }
